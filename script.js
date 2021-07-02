25bbce7ad60acd5b6b9e9ff101ca7403f5de2de6
Cristhiano Cunha <cristhiano.cunha@gmail.com>
2021-07-01 21:55:20 -0300

    inicio de javascript

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-alert("testando novamente");
-        
+var myNname = "cris"; // string
+
+var age = 27 // number
+
+var height = 1.70
+
+null
+undefined
+console.log(typeof myNname);
+var isStudent = false //boolean
+
+// objeto, array e function
+
+var person =[
+    "name": "Cris"
+    "age": 27
+]
+
+console.log(typeof myNname);
